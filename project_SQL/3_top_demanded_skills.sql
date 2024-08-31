08b4b48628856fae28514d90dfa854b5e3bebacd
Ahmed Magdy Mostafa Ali <117994460+hagoog@users.noreply.github.com>
2024-08-31 16:26:25 +0300

    fifth query

--- project_SQL/3_top_demanded_skills.sql
+++ project_SQL/3_top_demanded_skills.sql
@@ -16,4 +16,4 @@ INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE job_title_short = 'Data Analyst' AND job_work_from_home = TRUE
 GROUP BY skills
 ORDER BY demand_count DESC
-LIMIT 5 
+LIMIT 5
