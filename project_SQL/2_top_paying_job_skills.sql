c156f79405cffdf860995ad3bac7149fecabf543
Ahmed Magdy Mostafa Ali <117994460+hagoog@users.noreply.github.com>
2024-08-31 14:38:58 +0300

    second query

---
+++ project_SQL/2_top_paying_job_skills.sql
@@ -0,0 +1,150 @@
+/*
+Question: what are the required skills for the top-paying data analyst jobs?
+    -use the top 10 highest-paying data analyst kobs from the first query
+    -add the specific skills required for these roles.
+    -why? it provides a detailed look at which skills to develope that align with top salaries
+*/
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        salary_year_avg,
+        name AS company_name
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        LOWER(job_title_short) = LOWER('Data Analyst') AND
+        LOWER(job_location) = LOWER('Anywhere') AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT
+    top_paying_jobs.*,
+    skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC
+
+/*
+The dataset contains a list of job postings with the following columns:
+
+job_id: Identifier for each job posting.
+job_title: Title of the job.
+salary_year_avg: Average annual salary for the position.
+company_name: The company offering the job.
+skills: Required skills for the job.
+It appears that each row represents a skill associated with a specific job posting. I'll analyze the "skills" column to generate insights, such as the most common skills and any patterns in skill requirements across these top-paying data analyst roles. ​​
+
+Here are the top 10 most common skills found in the job postings:
+
+SQL - 8 occurrences
+Python - 7 occurrences
+Tableau - 6 occurrences
+R - 4 occurrences
+Snowflake - 3 occurrences
+Pandas - 3 occurrences
+Excel - 3 occurrences
+Azure - 2 occurrences
+Bitbucket - 2 occurrences
+Go - 2 occurrences
+These skills are frequently required in top-paying data analyst roles, with SQL, Python, and Tableau being the most prominent
+
+[
+  {
+    "job_id": 226942,
+    "job_title": "Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "650000.0",
+    "job_posted_date": "2023-02-20 15:13:33",
+    "company_name": "Mantys"
+  },
+  {
+    "job_id": 547382,
+    "job_title": "Director of Analytics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "336500.0",
+    "job_posted_date": "2023-08-23 12:04:42",
+    "company_name": "Meta"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset"
+  },
+  {
+    "job_id": 731368,
+    "job_title": "Director, Data Analyst - HYBRID",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189309.0",
+    "job_posted_date": "2023-12-07 15:00:13",
+    "company_name": "Inclusively"
+  },
+  {
+    "job_id": 310660,
+    "job_title": "Principal Data Analyst, AV Performance Analysis",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189000.0",
+    "job_posted_date": "2023-01-05 00:00:25",
+    "company_name": "Motional"
+  },
+  {
+    "job_id": 1749593,
+    "job_title": "Principal Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "186000.0",
+    "job_posted_date": "2023-07-11 16:00:05",
+    "company_name": "SmartAsset"
+  },
+  {
+    "job_id": 387860,
+    "job_title": "ERM Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "184000.0",
+    "job_posted_date": "2023-06-09 08:01:04",
+    "company_name": "Get It Recruit - Information Technology"
+  }
+]
+*/
